92a33cc8f18bfe0b321dc0a14b3a4ccfbcf54757
ff6347 <hash@ff6347.email>
2022-04-09 15:41:07 +0200

    feat(primitives)

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS 101
+
+Basics of Javascript as part of the seminar Generative Gestaltung in the summer semester 2022 at HAWK.de
---
+++ primitives.js
@@ -0,0 +1,35 @@
+//@ts-check
+// These are some primitive values returned by the functions. There are even more primitives https://developer.mozilla.org/en-US/docs/Glossary/Primitive
+// Actually there is no difference between an int and a float, but just for the fun of it we act like ther is one.
+
+// Make the function return the right thing
+
+function booleans() {
+  return undefined;
+}
+
+function ints() {
+  return undefined;
+}
+
+function floats() {
+  return undefined;
+}
+
+function numbers() {
+  return undefined;
+}
+
+function undefineds() {
+  return true;
+}
+
+function nulls() {
+  return undefined;
+}
+
+function strings() {
+  return undefined;
+}
+
+module.exports = { booleans, ints, numbers, floats, undefineds, nulls, strings };
