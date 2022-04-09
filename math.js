6bb46c6782f2a92e89d2d7876e5deb050069aae3
ff6347 <hash@ff6347.email>
2022-04-09 13:39:01 +0200

    feat(math): Adds math functions for replit test

--- math.js
+++ math.js
@@ -1,13 +1,50 @@
+// write basic arithmetic operations in javascript
+// add, subtract, multiply, devide
+// write a function that takes two numbers and returns the sum of them
+// write a function that takes two numbers and returns the difference of them
+// write a function that takes two numbers and returns the product of them
+// write a function that takes two numbers and returns the quotient of them
+
+// Bonus: write a function that takes two numbers and returns the remainder of them
+// hint take a look at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Remainder
+
+//@ts-check
+
 /**
- * Write a function that adds two values together
- * 
+ * @param {number} a
+ * @param {number} b
+ * @returns The sum of a and b
  */
-function add(){
-  
+function add(a, b) {
+  return a - b;
 }
 
-function subtract(){
-  
-}
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns The difference of a and b
+ */
+function subtract() {}
 
-module.exports = { add, subtract }
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns The product of a and b
+ */
+function multiply() {}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns The quotient of a and b
+ */
+function devide() {}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @returns The remainder of a and b
+ */
+function remainder() {}
+
+module.exports = { add, subtract, multiply, devide, remainder };
