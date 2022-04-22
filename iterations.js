11d0a3b0f9542882bdd26730464f74dbfc3a5cfa
Fabian Morón Zirfas <315106+ff6347@users.noreply.github.com>
2022-04-22 05:35:55 +0000

    docs: Add instructions to iterations

--- iterations.js
+++ iterations.js
@@ -1,9 +1,18 @@
 // @ts-check
+// Create some array functions
+// work with loops
+// make it dynamic
+// don't take the easy path by hardcoding return values
 
 /**
  * Create a function that takes no input but returns an Array where each value is 2 larger then the one before
  * @returns {number[]} Array of numbers with steps of two
  */
 function create2StepRange() {
-  return [];
+  return undefined;
+}
+
+
+module.exports = {
+  create2StepRange
 }
