6c0485ac6ce4ec1279b41059b247cc6f19f2a90d
ff6347 <hash@ff6347.email>
2022-04-22 07:14:39 +0200

    feat(iterations): Add function skeleton for 2 step arrray

---
+++ iterations.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * Create a function that takes no input but returns an Array where each value is 2 larger then the one before
+ * @returns {number[]} Array of numbers with steps of two
+ */
+function create2StepRange() {
+  return [];
+}
